06127a72ea63e144bfceb8a5a710358c494a59f1
wanxuan26 <WGAN011@e.ntu.edu.sg>
2026-04-04 20:11:34 +0800

    Add remove and clear history paths to URLs

--- search/urls.py
+++ search/urls.py
@@ -3,4 +3,6 @@ from search import views
 
 urlpatterns = [
     path('', views.search, name='search'),
+    path('remove-history/', views.remove_history, name='remove_history'),
+    path('clear-history/', views.clear_history, name='clear_history'),
 ]
